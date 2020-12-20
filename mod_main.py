0f55d6be2cb8656d5d988d75fd1c37152663b191
Dimosthenis Katsimardos <dimoskatsimardos@protonmail.com>
2020-12-20 21:27:41 +0200

    simplify the initialization with Python style ..

--- mod_main.py
+++ mod_main.py
@@ -22,7 +22,7 @@ class Diplo:
         Global Variables and lists to be used in this program
         BeWare these variables belong only to the class's scope !
     """
-    Nx = 500
+    Nx = 5 #500
     nsteps = 1200
     pi = 3.141593
     eps0 = float( 8.854 * pow(10,-12) )
@@ -32,39 +32,32 @@ class Diplo:
     dt = float( ddx/(2*c) )
     tc = 300
     t_0 = 90
-    ex = a1 = jx = Jxm1 = hy = eym1 = eym2 = omega_p = list()
+    ex = a1 = jx = Jxm1 = hy = eym1 = eym2 = omega_p = [[0] * Nx]
     def __init__(self):
         """
             Contructor for the Diplo class
         """
         self.init_all_lists()
 
-    def init_matrix(self, my_matrix):
-        """
-            A template simple funtion to initialize a matrix with zeros and size=500
-        """
-        for i in range(1, self.Nx+1):
-            my_matrix.append(0)
-
     def init_all_lists(self):
         """
-            A simple (void) bundle function that calls the template init_matrix() function to initialize
-            all needed lists that are declared in the beginning of this simple program
-            Create a list of all lists
-            and use this to initialize every list of this list
+            A simple (void) bundle function that demonstrates how matrices work in Python
+            so you can see and use later in your program ...
         """
         all_matrices = [self.ex, self.a1, self.jx, self.Jxm1, self.hy, self.eym1, self.eym2, self.omega_p]
+        all_matrices_names = ["ex", "a1", "jx", "Jxm1", "hy", "eym1", "eym2", "omega_p"]
         index = 0
         for item in all_matrices:
-            print("Initializing %d item from all_matrices list of lists ...\n" %(index))
-            self.init_matrix(item)
+            print("Now printing matrix : %s " %all_matrices_names[index])
             index = index + 1
+            for sub_item in item:
+                print("%s\n" %sub_item)
 
     def step_une(self):
         """
             The 1st step of the procedure to generate the matrices, calculate equations, etc...
         """
-        for i in range(100, 151):
+        for i in range(0,1): #(100, 151):
             self.a1[i]= (2- pow((omega_p[i] * dt), 2)/(1+dt*delta_p[i]) )
 
     def ret_ex_size(self):
@@ -78,9 +71,9 @@ def main():
     """
         main function
     """
-    print("main function called - starting main function ... ")
+    print("main function called - starting main function ... \n")
     DiploObject = Diplo()
-    print("Length of 'ex' list is: " + str(DiploObject.ret_ex_size()))
+    # print("Length of 'ex' list is: " + str(DiploObject.ret_ex_size()))
 
 if __name__ == "__main__":
     main()
