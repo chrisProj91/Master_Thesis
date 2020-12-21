9f41dc914ffa903042a30fb5aa947c774e6c9eeb
Dimosthenis Katsimardos <dimoskatsimardos@protonmail.com>
2020-12-21 11:35:42 +0200

    continue procedure a bit ..

--- mod_main.py
+++ mod_main.py
@@ -20,19 +20,32 @@ import math
 class Diplo:
     """ 
         Global Variables and lists to be used in this program
-        BeWare these variables belong only to the class's scope !
+        Use these variables only inside the class's scope/functions !
     """
-    Nx = 5 #500
+    Nx = 5  # 500
     nsteps = 1200
-    pi = 3.141593
     eps0 = float( 8.854 * pow(10,-12) )
-    m0 = float( 4 * pi * pow(10, -7) )
+    m0 = float( 4 * math.pi * pow(10, -7) )
     c = float( 1/math.sqrt(eps0*m0) )
     ddx = float( pow(10, -4) )
     dt = float( ddx/(2*c) )
     tc = 300
     t_0 = 90
-    ex = a1 = jx = Jxm1 = hy = eym1 = eym2 = omega_p = [[0] * Nx]
+    # Initialize all lists/matrices with Nx*zeros or Nx*ones respectively
+    #
+    ex = jx = Jxm1 = hy = eym1 = eym2 = omega_p = sigma = omega_zero = delta_p = f = p = [[0] * Nx]
+    a1 = a2 = a3 = [[0] * Nx]
+    c1 = [[0] * Nx]
+    c2 = [[1] * Nx]
+    # Cell size
+    ddx = 0.01
+    # Time step size | dt = dx/(6*10^8)
+    dt = float(ddx/(6*pow(10, 8)))
+    # Parameter medium Lorentz
+    eps_inf = 1.0
+    # eps-eps_inf | Deps_p = 0.5
+    deps_p = 0.5
+
     def __init__(self):
         """
             Contructor for the Diplo class
@@ -44,8 +57,10 @@ class Diplo:
             A simple (void) bundle function that demonstrates how matrices work in Python
             so you can see and use later in your program ...
         """
-        all_matrices = [self.ex, self.a1, self.jx, self.Jxm1, self.hy, self.eym1, self.eym2, self.omega_p]
-        all_matrices_names = ["ex", "a1", "jx", "Jxm1", "hy", "eym1", "eym2", "omega_p"]
+        all_matrices = [self.ex, self.a1, self.jx, self.Jxm1, self.hy, self.eym1, self.eym2, self.omega_p, \
+                        self.sigma, self.omega_zero, self.delta_p, self.f, self.p, self.a2, self.a3]
+        all_matrices_names = ["ex", "a1", "jx", "Jxm1", "hy", "eym1", "eym2", "omega_p", \
+                               "sigma", "omega_zero", "delta_p", "f", "p", "a2", "a3"]
         index = 0
         for item in all_matrices:
             print("Now printing matrix : %s " %all_matrices_names[index])
@@ -53,11 +68,15 @@ class Diplo:
             for sub_item in item:
                 print("%s\n" %sub_item)
 
+    def fill_c3(self):
+        for k in range(1, Nx):
+            c3 = float(dt/eps0)
+
     def step_une(self):
         """
             The 1st step of the procedure to generate the matrices, calculate equations, etc...
         """
-        for i in range(0,1): #(100, 151):
+        for i in range(100, 151):
             self.a1[i]= (2- pow((omega_p[i] * dt), 2)/(1+dt*delta_p[i]) )
 
     def ret_ex_size(self):
